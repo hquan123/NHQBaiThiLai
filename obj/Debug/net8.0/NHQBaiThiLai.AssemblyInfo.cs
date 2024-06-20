2b0d9fa5a5bbb691cb5145de88178b729110fb2a
“hquan123 <hquan16102002@gmial.com>
2024-06-20 23:44:45 +0700

    NHQ542_Cau1

--- obj/Debug/net8.0/NHQBaiThiLai.AssemblyInfo.cs
+++ obj/Debug/net8.0/NHQBaiThiLai.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NHQBaiThiLai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d287f8eb0a1feb95f0b5b34c8b0a7210073b5d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("NHQBaiThiLai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NHQBaiThiLai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
